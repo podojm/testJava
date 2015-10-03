e6efa356b614929ff347433d7e6c9937d2b7cb22
songjimin <gaejm17@gmail.com>
2015-10-03 18:04:42 +0900

    Git Test Code

---
+++ src/test01/HelloWorld.java
@@ -0,0 +1,10 @@
+package test01;
+
+/**
+ * Created by songjimin on 2015. 10. 3..
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("기도하자");
+    }
+}
